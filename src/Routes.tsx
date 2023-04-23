b3fc85ca5922bee8a8ccc17cce044839a89df653
Camillo Rinaldi <camillo@kxptech.com>
2023-04-23 18:32:44 -0300

    testing going to terms of use without redirect

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -15,7 +15,6 @@ export default function AppRoutes() {
         <Route path="/LNbits" element={<Home />} />
         <Route path="/LNbits/terms-of-use" element={<TermsOfUse />} />
       </Routes>
-      <RedirectHandler />
     </Router>
   );
 }
